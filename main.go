6f30b207792ebfbdab956b0748999f6df10a9f6a
cyh <2263463374@qq.com>
2024-10-17 21:09:48 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	ratelimit1 "go.uber.org/ratelimit" //漏桶
+)
+
+func pingHandler(c *gin.Context) {
+	c.String(http.StatusOK, "pong")
+}
+
+func heiHandler(c *gin.Context) {
+	c.String(http.StatusOK, "ha")
+}
+
+// 基于漏桶的限流中间件1
+func rateLimit1() func(ctx *gin.Context) {
+	//生成一个限流器
+	rl := ratelimit1.New(100)
+
+	return func(c *gin.Context) {
+		if rl.Take().Sub(time.Now()) > 0 {
+			//time.Sleep(rl.Take().Sub(time.Now()))//需要等这么长时间下一滴水才会滴下来
+			c.String(http.StatusOK, "rate limit...")
+			c.Abort()
+			return
+		} //取水滴
+		c.Next()
+	}
+}
+
+// 基于令牌桶的限流中间件2
+func rateLimit2() func(ctx *gin.Context) {
+	return func(c *gin.Context) {
+		rateLimit2.NewBucketWithRate(2, 5)
+	}
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", rateLimit1(), pingHandler)
+	r.GET("/hei", rateLimit2(), heiHandler)
+
+	r.Run()
+}
